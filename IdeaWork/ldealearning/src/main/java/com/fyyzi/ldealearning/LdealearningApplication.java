d6ecc81fc62747280bfba7d8876cae7ff6da8f9a
xiyang <xiyang_job@126.com>
2017-12-26 11:25:12 +0800

    Create IdeaLearning Project
    Signed-off-by: xiyang <e0001857@shenhua.cc>

---
+++ IdeaWork/ldealearning/src/main/java/com/fyyzi/ldealearning/LdealearningApplication.java
@@ -0,0 +1,12 @@
+package com.fyyzi.ldealearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdealearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdealearningApplication.class, args);
+	}
+}
---
+++ IdeaWork/ldealearning/src/test/java/com/fyyzi/ldealearning/LdealearningApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fyyzi.ldealearning;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LdealearningApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
