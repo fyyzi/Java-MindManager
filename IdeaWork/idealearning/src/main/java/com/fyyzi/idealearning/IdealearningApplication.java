3edd90f512974b19b1b882ec879812cff100d240
xiyang <e0001857@shenhua.cc>
2017-12-26 14:27:42 +0800

    Create

---
+++ IdeaWork/idealearning/src/main/java/com/fyyzi/idealearning/IdealearningApplication.java
@@ -0,0 +1,12 @@
+package com.fyyzi.idealearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdealearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdealearningApplication.class, args);
+	}
+}
