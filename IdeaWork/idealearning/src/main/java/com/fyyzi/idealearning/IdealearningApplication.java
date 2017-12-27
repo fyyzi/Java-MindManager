9513894e05b29c2fdd8982ca92fd198caf54fa23
xiyang <e0001857@shenhua.cc>
2017-12-27 12:06:55 +0800

    添加作者信息   以符合阿里规约
    添加X-notes 分支  该分支作为笔记分支

--- IdeaWork/idealearning/src/main/java/com/fyyzi/idealearning/IdealearningApplication.java
+++ IdeaWork/idealearning/src/main/java/com/fyyzi/idealearning/IdealearningApplication.java
@@ -5,11 +5,19 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+/**
+ * Spring Boot 启动方法
+ *
+ * @author 息阳
+ * 2017/12/26 14:51
+ * @version 1.0
+ */
+@SuppressWarnings("AlibabaClassMustHaveAuthor")
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
 public class IdealearningApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IdealearningApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(IdealearningApplication.class, args);
+    }
 }
