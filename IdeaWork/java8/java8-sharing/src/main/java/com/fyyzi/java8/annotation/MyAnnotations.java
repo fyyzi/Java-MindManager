e9e816cd35bf0ecfd58ac8391208e77c9a5526e3
xiyangPC <a0dream@live.com>
2018-01-05 22:25:56 +0800

    update JDK1.8

---
+++ IdeaWork/java8/java8-sharing/src/main/java/com/fyyzi/java8/annotation/MyAnnotations.java
@@ -0,0 +1,22 @@
+package com.fyyzi.java8.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.LOCAL_VARIABLE;
+
+/**
+ * @author 息阳
+ * Create in 22:12 2018/1/5
+ * @version 1.0
+ */
+@Target({TYPE, FIELD, METHOD, PARAMETER, CONSTRUCTOR, LOCAL_VARIABLE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MyAnnotations {
+
+    MyAnnotation[] value();
+
+}
