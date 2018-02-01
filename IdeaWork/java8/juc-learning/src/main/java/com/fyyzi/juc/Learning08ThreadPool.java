4ca50525787d05d62a594c930cc336e63c08ef49
xiyang <e0001857@shenhua.cc>
2018-02-01 17:09:01 +0800

    Create 线程池

---
+++ IdeaWork/java8/juc-learning/src/main/java/com/fyyzi/juc/Learning08ThreadPool.java
@@ -0,0 +1,12 @@
+package com.fyyzi.juc;
+
+/**
+ * 一、线程池：提供了一个线程队列，队列中保存着所有等待状态的线程。避免了创建与销毁线程的额外开销，提高了响应的速度。
+ *
+ * @author 息阳
+ * 2018/2/1 17:06
+ * @version 1.0
+ */
+public class Learning08ThreadPool {
+
+}
