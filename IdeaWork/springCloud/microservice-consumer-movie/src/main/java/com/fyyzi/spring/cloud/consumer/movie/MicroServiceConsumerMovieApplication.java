c7d678ba6798b05afe5d23ff54d5214ce93d49f7
xiyang_homePC <xiyang_job@126.com>
2018-05-28 18:59:00 +0800

    update

--- IdeaWork/springCloud/microservice-consumer-movie/src/main/java/com/fyyzi/spring/cloud/consumer/movie/MicroServiceConsumerMovieApplication.java
+++ IdeaWork/springCloud/microservice-consumer-movie/src/main/java/com/fyyzi/spring/cloud/consumer/movie/MicroServiceConsumerMovieApplication.java
@@ -1,9 +1,11 @@
 package com.fyyzi.spring.cloud.consumer.movie;
 
+import com.fyyzi.spring.config.TestConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -12,6 +14,9 @@ import javax.swing.*;
 
 @SpringBootApplication
 @EnableEurekaClient
+
+// 若采用配置文件配置Ribbon负载均衡，则优先级高于@RibbonClient 注解
+@RibbonClient(name = "MICROSERVICE-PROVIDER-USER", configuration = TestConfiguration.class)
 public class MicroServiceConsumerMovieApplication {
 
     @Bean
--- IdeaWork/springCloud/microservice-consumer-movie/src/main/java/com/fyyzi/spring/cloud/consumer/movie/controller/MovieController.java
+++ IdeaWork/springCloud/microservice-consumer-movie/src/main/java/com/fyyzi/spring/cloud/consumer/movie/controller/MovieController.java
@@ -2,6 +2,8 @@ package com.fyyzi.spring.cloud.consumer.movie.controller;
 
 import com.fyyzi.spring.cloud.consumer.movie.entitys.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +18,23 @@ public class MovieController {
     @RequestMapping("/movie/{id}")
     public User findById(@PathVariable Long id) {
         return restTemplate.getForObject("http://MICROSERVICE-PROVIDER-USER/simple/" + id, User.class);
+    }
+
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+    @RequestMapping("/test")
+    public String test() {
+
+        ServiceInstance serviceInstance = this.loadBalancerClient.choose("MICROSERVICE-PROVIDER-USER");
+
+        System.out.println(serviceInstance.getHost() + ":" + serviceInstance.getPort() + ":" + serviceInstance.getServiceId());
+
+        ServiceInstance serviceInstance2 = this.loadBalancerClient.choose("MICROSERVICE-PROVIDER-USER2");
+        System.err.println(serviceInstance2.getHost() + ":" + serviceInstance2.getPort() + ":" + serviceInstance2.getServiceId());
+
+
+        return "1";
     }
 }
